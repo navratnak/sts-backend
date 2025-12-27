ac1c593dba587123a707a7aae41315a8bda9b1fa
navratnak <101383096+navratnak@users.noreply.github.com>
2025-12-27 15:36:28 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 // \backend\src\app.js
 
 import express from "express";
-import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -15,27 +14,36 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 app.set("trust proxy", 1);
+
 const allowedOrigins = [
   "https://360websolution.es",
   "https://360websolution.es/admin",
 ];
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow server-to-server & Postman
-      if (!origin) return callback(null, true);
 
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,PATCH,DELETE,OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
+
 app.options("*", cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
