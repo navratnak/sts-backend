6212a4e9692e9c62cb76c0a58cca3390b37501e4
navratnak <101383096+navratnak@users.noreply.github.com>
2025-12-27 12:09:21 +0530

    Update db.js
    '

--- src/config/db.js
+++ src/config/db.js
@@ -4,9 +4,6 @@ import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config();
 
-console.log("DB USER:", process.env.DB_USER);
-console.log("DB PASS:", process.env.DB_PASSWORD);
-
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
